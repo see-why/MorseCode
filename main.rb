bd306fbd3f785d7667d97696df8138d2f8e8d114
see-why <iyadicy@gmail.com>
2021-12-08 18:24:42 +0100

    fix linters errors

--- main.rb
+++ main.rb
@@ -1,56 +1,30 @@
-$morse = { 
-".-" => "A",
-"-..." => "B",
-"-.-." => "C",
-"-.." => "D",
-"." => "E",
-"..-." => "F",
-"--." => "G",
-"...." => "H",
-".."=> "I",
-".---" => "J",
-"-.-" => "K",
-".-.." => "L",
-"--" => "M",
-"-." => "N",
-"---" => "O",
-".---." => "P",
-"--.-" => "Q",
-".-." => "R",
-"..." => "S",
-"-" => "T",
-"..-" => "U",
-"...-" => "V",
-".--" => "W",
-"-..-" => "X",
-"-.--" => "Y",
-"--.." => "Z",
-"/" => " ",
-}
-
 def decode_char(char)
-  $morse[char]
+  morse = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+            '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K',
+            '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O', '.---.' => 'P', '--.-' => 'Q',
+            '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U', '...-' => 'V', '.--' => 'W',
+            '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z', '/' => ' ' }
+  morse[char]
 end
 
 def decode_word(word)
-  array0fCharacters = word.split(" ");
-  decodedWord = '';
-  array0fCharacters.each do |char|
-    decodedWord += decode_char(char);
+  array_of_characters = word.split
+  decoded_word = ''
+  array_of_characters.each do |char|
+    decoded_word += decode_char(char)
   end
-  return decodedWord
+  decoded_word
 end
 
-
 def decode_message(message)
-  array0fCharacters = message.split("   ");
-  decodedWord = [];
-  array0fCharacters.each do |char|
-    decodedWord.push(decode_word(char));
+  array_of_words = message.split('   ')
+  decoded_message = []
+  array_of_words.each do |word|
+    decoded_message.push(decode_word(word))
   end
-  return decodedWord.join(' ')
+  decoded_message.join(' ')
 end
 
 puts decode_char('...')
-puts decode_word("-- -.--")
-puts decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
+puts decode_word('-- -.--')
+puts decode_message('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
